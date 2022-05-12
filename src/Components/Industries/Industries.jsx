1f80d648509858b46d72373b576a460c66c73d95
josephenoch <enochjoseph01@gmail.com>
2022-05-12 10:33:02 +0100

    :lipstick: update the industries section to be mobile responsive

--- src/Components/Industries/Industries.jsx
+++ src/Components/Industries/Industries.jsx
@@ -42,11 +42,12 @@ export const Industries = () => {
   ]
   return (
     <>
-        <div className="w-1/3 flex flex-col items-center space-y-3 mt-5">
-            <p className="uppercase font-bold text-lg">Browse by <span className="text-theme">industries</span></p>
+        <div className="w-1/2 lg:w-1/3 flex flex-col items-center space-y-7 lg:space-y-3 mt-5">
+            <p className="uppercase font-bold text-2xl text-center lg:hidden">Industries We <span className="text-theme">Hire</span> For</p>
+            <p className="uppercase font-bold text-lg hidden lg:block">Browse by <span className="text-theme">industries</span></p>
             <p className='text-xs text-center font-medium'>Lorem ipsum dolor sit, amet consectetur adipisicing elit. Deleniti praesentium harum perspiciatis ea eum sunt sint, reprehenderit</p>
         </div>
-        <div className="flex flex-wrap w-[70%] justify-between">
+        <div className="flex lg:flex-wrap flex-col mt-10 lg:mt-0 lg:flex-row w-full lg:w-[70%] lg:justify-between items-center lg:items-start">
             {
                 industries.map((industry, index)=><IndustryCard key={index} industry={industry}/>)
             }
