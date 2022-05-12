991f0584db69ad5d55cf31f1a5e0c766484b6b08
josephenoch <enochjoseph01@gmail.com>
2022-05-12 10:43:45 +0100

    :lipstick: update join pipeline section to be mobile responsive

--- src/Components/JoinPipeline/JoinPipeline.jsx
+++ src/Components/JoinPipeline/JoinPipeline.jsx
@@ -8,14 +8,17 @@ export const JoinPipeline = () => {
   const benefits = [1,2,3,4,5]
   return (
       <>
-        <div className="w-4/5 flex h-full">
-            <img src={JoinPipelineImage} alt="Join Pipeline" className="w-1/2 h-full rounded-l-xl" />
-            <div className="w-1/2 h-full p-16 pt-5 bg-[#f5f5f5] rounded-r-xl flex flex-col justify-between">
-                <p className="text-3xl font-bold mt-16 ">Why You Should Join the <br/><span className="text-theme">Nerdeye Talent Pipeline</span> </p>
-                <div className="w-full space-y-10">
+      <div className="lg:hidden mb-16 -mt-16">
+          <Signature/>
+      </div>
+        <div className="w-[90%] lg:w-4/5 flex h-full">
+            <img src={JoinPipelineImage} alt="Join Pipeline" className="w-1/2 h-full rounded-l-xl hidden lg:block" />
+            <div className="w-full lg:w-1/2 h-full p-5  lg:p-16 pt-5 bg-[#f5f5f5] rounded-xl lg:rounded-l-none flex flex-col justify-between">
+                <p className="text-3xl font-bold mt-8 lg:mt-16 text-center lg:text-left ">Why You Should Join the <br/><span className="text-theme">Nerdeye Talent Pipeline</span> </p>
+                <div className="w-full lg:block flex flex-col items-center lg:space-y-10 mt-10 lg:mt-0">
                     {
                         benefits.map(benefit=>
-                            <div key={benefit} className="w-full h-12 bg-[#e8e8e8] rounded-2xl flex p-7 space-x-5 items-center">
+                            <div key={benefit} className=" w-3/4 lg:w-full h-12 lg:bg-[#e8e8e8] rounded-2xl flex p-7 space-x-5 items-center">
                                 <div className="h-8 w-8 bg-[#c4c4c4] rounded-full flex justify-center align center">
                                     <img src={TickIcon} alt="Good" className="w-3"/>
                                 </div>
